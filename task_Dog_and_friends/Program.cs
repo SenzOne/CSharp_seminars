47211e0abb43375cd7ee83e8bacdd5c36a0cd9a8
Semen Sl <sem63sam@yandex.ru>
2023-01-26 13:35:23 +0400

    Added solutions to problems from the first workshop

---
+++ task_Dog_and_friends/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0,
+    distance = 10000,
+    first_frend_speed = 1,
+    second_frend_speed = 2,
+    dog_speed = 5,
+    frend = 2,
+    time = 0;
+
+while (distance > 10)
+{
+    if (frend == 1)
+    {
+        time = distance / (first_frend_speed + dog_speed);
+        frend = 2;
+        Console.WriteLine($"{count} От 1 >>> к 2 Distance: {distance}");
+    }
+    else
+    {
+        time = distance / (second_frend_speed + dog_speed);
+        frend = 1;
+        Console.WriteLine($"{count} От 2 >>> к 1 Distance: {distance}");
+    }
+    distance = distance - (first_frend_speed + second_frend_speed) * time;
+    count++;
+}
+Console.WriteLine($"Count: {count}");
